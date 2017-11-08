12eecf28a8c1309f4fdce78874b28e84c3bdb36f
Apple <Apple@MacBook-Air-de-Apple-2.local>
2017-11-08 15:42:09 -0600

    2

--- tres/TresAlfaViewController.swift
+++ tres/TresAlfaViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TresAlfaViewController.swift
 //  tres
 //
 //  Created by Apple on 08/11/17.
@@ -8,19 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TresAlfaViewController: UIViewController {
     
-
+    @IBOutlet weak var manitas: UIButton!
+    
+    @IBOutlet weak var scoreM: UILabel!
+    
+    @IBOutlet weak var scoreP: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    @IBAction func puño(_ sender: Any) {
+    }
+   
 
 }
-
